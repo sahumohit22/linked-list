682911724c8e12f1529b2a125c6e60bebfbb553c
Suyash <suyash.srivastava96@gmail.com>
2018-10-05 02:24:41 +0530

    Add function to add element at any index in a linked list

--- LinkedList.py
+++ LinkedList.py
@@ -16,6 +16,32 @@ class LinkedList:
 				temp = temp.next
 			temp.next = new_node
 
+	# position is 0 based
+	def add_at(self, data, position):
+		new_node = ListNode(data)
+
+		if position < 0:
+			print("Invalid position - " + str(position))
+			return
+
+		if position is 0:
+			new_node.next = self.head
+			self.head = new_node
+			return
+
+		temp = self.head
+		count = 1
+		while temp is not None and count < position:
+			temp = temp.next
+			count += 1
+
+		if count is not position or temp is None:
+			print("Invalid position - " + str(position))
+		else:
+			current_next = temp.next
+			temp.next = new_node
+			new_node.next = current_next
+
 	def print_list(self):
 		temp = self.head
 
--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from LinkedList import LinkedList
 
-print(3)
-print(dir())
-
 linked_list = LinkedList()
+
 linked_list.add(2)
 linked_list.add(4)
 linked_list.add(6)
 linked_list.add(1)
-linked_list.print()
+
+linked_list.print_list()
+print("========================")
+
+linked_list.add_at(10, 1)
+
+linked_list.print_list()
+print("========================")
+
+linked_list.add_at(20, 3)
+
+linked_list.print_list()
+print("========================")
+
+linked_list.add_at(50, 0)
+linked_list.print_list()
+print("========================")
+
+linked_list.add_at(100, 7)
+linked_list.print_list()
+print("========================")
